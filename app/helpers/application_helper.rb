21e50b265f03fd11be5bc6cd6cebd179e9dbbe3a
Chris <me@chrishoenack.com>
2016-05-13 00:04:23 +0000

    Add Age Field and remove_unwanted_words ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    
+  def remove_unwanted_words(string)
+    bad_words = ["less than", "about", "over", "almost"]
+    
+    bad_words.each do |bad|
+      string.gsub!(bad + " ", '')
+    end
+    
+    return string
+  end
+    
 end
