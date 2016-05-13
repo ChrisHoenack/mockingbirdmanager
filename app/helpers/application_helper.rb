5095852cad0bacba7dca9892903b9e4c6d6b1b46
Chris <me@chrishoenack.com>
2016-05-13 00:13:09 +0000

    Fix Age Feature to Show Correct Date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,8 @@
 module ApplicationHelper
-    
-  def remove_unwanted_words(string)
-    bad_words = ["less than", "about", "over", "almost"]
-    
-    bad_words.each do |bad|
-      string.gsub!(bad + " ", '')
-    end
-    
-    return string
+  
+  def find_age(birthday)
+    now = Time.now.utc.to_date
+    now.year - birthday.year - (birthday.to_date.change(:year => now.year) > now ? 1 : 0)
   end
     
 end
